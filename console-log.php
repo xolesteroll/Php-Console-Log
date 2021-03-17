b9c2ee547c31b988e1f2ec23e84968fa97619c5f
Anton <67501086+xolesteroll@users.noreply.github.com>
2021-03-17 11:14:34 +0200

    styling

--- console-log.php
+++ console-log.php
@@ -1,11 +1,11 @@
 function console_log($data){ 
     if(is_array($data) || is_object($data)){
-		echo("<script>console.log('php_array: ".json_encode($data)."');</script>");
-	} elseif($data === true) {
+	echo("<script>console.log('php_array: ".json_encode($data)."');</script>");
+    } elseif($data === true) {
         echo("<script>console.log('true');</script>");
     } elseif($data === false) {
         echo("<script>console.log('false');</script>");
     }else {
-		echo("<script>console.log('php_string: ".$data."');</script>");
-	}
+	echo("<script>console.log('php_string: ".$data."');</script>");
+    }
 }
